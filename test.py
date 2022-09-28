4df074e17a29f7c28bbc19e3d22d1b35a5b5aa1e
Baladitya Swaika <swaika@usc.edu>
2022-09-27 21:42:42 -0700

    Inital commit.

---
+++ test.py
@@ -0,0 +1,20 @@
+import rosbag
+
+class Point:
+    def __init__(self, x, y, z, r, g, b, a) -> None:
+        self.location = (x, y, z)
+        self.color = (r, g, b, a)
+    
+    def __str__(self) -> str:
+        return f'Point(location={str(self.location)}, color={str(self.color)}'
+
+bag = rosbag.Bag('./crose115.bag')
+starts = {i:[] for i in range(1, 116)}
+ends = {i:[] for i in range(1, 116)}
+for msg in bag.read_messages():
+    params = dict(map(lambda x: (x[0].strip(), x[1].strip()), filter(lambda x: len(x) > 1 and x[1] != '', (map(lambda x: x.strip().split(':'), str(msg.message).split('\n'))))))
+    point = Point(float(params['x']), float(params['y']), float(params['z']), float(params['r']), float(params['g']), float(params['b']), float(params['a']))
+    starts[int(params['start'])].append(point)
+    ends[int(params['end'])].append(point)
+
+print(len(starts[1]))
